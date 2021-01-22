8ad87f37e8aecf2a921291a90cdd6a282c58e763
mzstevens69 <stevensmike36@yahoo.com>
2021-01-22 11:20:17 -0600

    added .env

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import gspread
+from twitter import *
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 gc = gspread.service_account('credentials.json')
 
+token = os.environ.get("token") 
+token_secret = os.environ.get("token_secret")
+consumer_key = os.environ.get("consumer_key") 
+consumer_secret = os.environ.get("consumer_secret")
+
+t = Twitter(
+    auth=OAuth(token, token_secret, consumer_key, consumer_secret))
+
+
 # Open a sheet from a spreadsheet in one go
+
 wks = gc.open("theSocialGamePokerclub").sheet1
 
 # Update a range of cells using the top left corner address
 # wks.update('A1', [[1, 2], [3, 4]])
+
 next_tweet = wks.acell('A2').value
 
 #post tweet through twitterAPI
 
+t.statuses.update(status=next_tweet)
+
 #delete row on success
+
 wks.delete_rows(2)
 
 # Or update a single cell
